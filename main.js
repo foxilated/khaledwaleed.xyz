b9334e7cca61ffc2b89289f93ff68728d9187f58
Khaled Waleed <foxilated@pm.me>
2021-12-05 16:41:59 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function stillWorking() {
+    alert("Still working on that.");
+}
+// Preloader
+var preloader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+    preloader.style.display = "none"
+})
