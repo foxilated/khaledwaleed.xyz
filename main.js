d8ae07c028c78c7751dad66fdb0a7f8e4dbf08ed
Khaled Waleed <foxilated@pm.me>
2021-12-07 18:29:25 +0300

    idk

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function stillWorking() {
-  alert("Still working on that.");
+  alert('Still working on that.');
 }
 // Preloader
-var preloader = document.getElementById("preloader");
-window.addEventListener("load", function () {
-  preloader.style.display = "none";
+var preloader = document.getElementById('preloader');
+window.addEventListener('load', function () {
+  preloader.style.display = 'none';
 });
