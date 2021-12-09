94085947c6b2a1f3c27b29d5dc06fea1bb0abd5b
Khaled Waleed <foxilated@pm.me>
2021-12-09 21:20:51 +0300

    1

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,6 @@
 function stillWorking() {
   alert('Still working on that.');
 }
-function play() {
-  var audio = document.getElementById('audio');
-  audio.play();
-}
 // Preloader
 const preloader = document.getElementById('preloader');
 window.addEventListener('load', function () {
