b0e1fef57bd3c3777844ce98282f1248ce1498fe
Khaled Waleed <foxilated@pm.me>
2021-12-09 20:24:07 +0300

    sound effects

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
+// Under Construction Alert
 function stillWorking() {
   alert('Still working on that.');
+}
+// Click Sound
+function play() {
+  var audio = document.getElementById('audio');
+  audio.play();
 }
 // Preloader
 var preloader = document.getElementById('preloader');
 window.addEventListener('load', function () {
   preloader.style.display = 'none';
+  play();
 });
