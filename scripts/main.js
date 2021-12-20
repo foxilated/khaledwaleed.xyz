cd05317ddc25adbccdd1979e094a48f32b797088
Khaled Waleed <foxilated@pm.me>
2021-12-20 11:17:04 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ function stillWorking() {
   alert('Still working on that.');
 }
 // Preloader
-const preloader = document.getElementById('preloader');
-window.addEventListener('load', function () {
-  preloader.style.display = 'none';
-});
+// const preloader = document.getElementById('preloader');
+// window.addEventListener('load', function () {
+//   preloader.style.display = 'none';
+// });
